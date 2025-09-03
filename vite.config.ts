27f82d0ff6a57c7305a1ec8e8516babbafb094b2
Hondo-Reilly <hondoreilly@gmail.com>
2025-09-03 09:48:51 -0500

    pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/' // correct for a root domain (custom domain)
+  base: '/hreilly/' // correct for a root domain (custom domain)
 })
