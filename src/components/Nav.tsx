1a12d2074ade1e7e391924d7ae9cb75d454f5c4a
Hondo-Reilly <hondoreilly@gmail.com>
2025-09-09 16:47:38 -0500

    auto scroll fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,6 @@ export default function Nav() {
                 <a href="#experience">Experience</a>
                 <a href="#projects">Projects</a>
                 <a href="#contact">Contact</a>
-
             </div>
         </div>
     </nav>
